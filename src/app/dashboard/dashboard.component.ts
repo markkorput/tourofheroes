5e6beb8c2907343a0c1d9200af32c9714040c27c
Mark van de Korput <m.vandekorput@eaglescience.nl>
2020-09-02 14:56:52 +0200

    Dashboard initialising message implementation

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,9 +11,12 @@ import { HeroService } from '../hero.service';
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) { }
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService) { }
 
   ngOnInit() {
+    this.messageService.add('Dashboard initialising...');
     this.getHeroes();
   }
 
