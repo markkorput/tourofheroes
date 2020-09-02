e27932c3111bacb90014382493e990c2a5e5e969
Mark van de Korput <m.vandekorput@eaglescience.nl>
2020-09-02 17:07:08 +0200

    comments

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -21,17 +21,22 @@ describe('HeroService', () => {
 
   describe('getHero', () => {
     it ('should fetch hero details', (done) => {
-    
+      // Call the method and subscribe a result handler
       heroService.getHero(123).subscribe(data => {
         expect(data).toEqual({id: 125, name: "One-Two-Five"});
         const messages = TestBed.get(MessageService).messages;
         expect(messages[messages.length-1]).toEqual(`HeroService: fetched hero id=123`);
+
+        // indicate this async test is finished
         done();
       });
-    
+
+      // specify and stub the HTTP request that we expect to be performed
       const req = http.expectOne('api/heroes/123');
+      // mock response data that should be returned
       req.flush({id: 125, name: "One-Two-Five"});
 
+      // verify that no other HTTP requests were performed
       http.verify();
     });
   });
